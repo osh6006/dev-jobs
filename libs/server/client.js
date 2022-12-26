a9d7d27f73f96c74c505fb7d6763f1040a13a8be
SEOK <ohs6006@gmail.com>
2022-12-26 14:18:19 +0900

    데이터 스키마 재정의

--- libs/server/client.js
+++ libs/server/client.js
@@ -1,3 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+const client = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") {
+  global.client = client;
+}
+
+export default client;
